c2304d5f4627364bfdbc73f32003e779521fa07e
Parvez Dittia <parvezdittia@gmail.com>
2020-06-03 21:19:26 +0530

    Basic react setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+    render(){
+
+        return <span>Hello World</span>
+
+    }
+
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
