53e97715f18600be2cf57f84659cb09a3d96e232
Parvez Dittia <parvezdittia@gmail.com>
2020-06-08 20:15:54 +0530

    Integrate eslint into workflow

--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ReactIf from './ReactIf';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
+		this.state = {
+			flag: true,
+		};
+	}
 
-        super(props);
-
-        this.state = {
-
-            flag: true
-
-        }
-
-    }
-
-
-    render(){
-
-        return (
-
-            <ReactIf when={this.state.flag}>
-
-                <span>Simple react if</span>
-
-            </ReactIf>
-
-        )
-
-    }
-
-
+	render() {
+		return (
+			<ReactIf when={this.state.flag}>
+				<span>Simple react if</span>
+			</ReactIf>
+		);
+	}
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
